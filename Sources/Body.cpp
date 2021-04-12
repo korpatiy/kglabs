c1fe596e5575306439906ae83715a32f60c91d4d
korpatiy <tretiakovslava@gmail.com>
2021-04-12 14:29:23 +0500

    full worked

--- Sources/Body.cpp
+++ Sources/Body.cpp
@@ -80,10 +80,83 @@ bool Body::createModel(const std::vector<Point2D> &points) {
     const int rev = 128;
 
     const int n = points.size();
-    const int vertSize = 6 * n * (rev + 1);
-    const int idxSize = 6 * n * rev;
+    const int vertSize = 6 * n * rev;
+    const int idxSize = 6 * (n - 1) * rev;
+    auto *vertices = new GLfloat[vertSize];
+    auto *indices = new GLuint[idxSize];
 
-    for (int i = 1; i < points.size() - 1; i++) {
+    auto v = points[1] - points[0];
+    v.normalize();
+    vertices[0] = points[0].x;
+    vertices[1] = points[0].y;
+    vertices[2] = 0.0f;
+    vertices[3] = -v.y;
+    vertices[4] = v.x;
+    vertices[5] = 0.0f;
+
+    v = points[n - 2] - points[n - 1];
+    v.normalize();
+    int step = 6 * n;
+    vertices[step - 6] = points[n - 1].x;
+    vertices[step - 5] = points[n - 1].y;
+    vertices[step - 4] = 0.0f;
+    vertices[step - 3] = v.y;
+    vertices[step - 2] = -v.x;
+    vertices[step - 1] = 0.0f;
+
+    for (int i = 6, k = 1; i < (n - 1) * 6; i += 6, k++) {
+        auto v1 = points[k] - points[k - 1];
+        v1.normalize();
+        auto v2 = points[k + 1] - points[k];
+        v2.normalize();
+        auto normal = v1 + v2;
+        normal.normalize();
+        vertices[i] = points[k].x;
+        vertices[i + 1] = points[k].y;
+        vertices[i + 2] = 0.0f;
+        vertices[i + 3] = -normal.y;
+        vertices[i + 4] = normal.x;
+        vertices[i + 5] = 0.0f;
+    }
+
+    auto model = glm::mat4(1.0f);
+    auto rad = 360.0f / rev;
+    auto rotated = glm::rotate(model, glm::radians(rad), glm::vec3(1.0, 0.0, 0.0));
+    for (int i = step; i < vertSize; i += step) {
+        for (int j = 0; j < step; j += 6) {
+            size_t idx = i + j - step;
+            auto resultPos = rotated * glm::vec4(vertices[idx], vertices[idx + 1], vertices[idx + 2], 1.0f);
+            auto resultNormal = rotated * glm::vec4(vertices[idx + 3], vertices[idx + 4], vertices[idx + 5], 1.0f);
+            vertices[i + j] = resultPos.x;
+            vertices[i + j + 1] = resultPos.y;
+            vertices[i + j + 2] = resultPos.z;
+            vertices[i + j + 3] = resultNormal.x;
+            vertices[i + j + 4] = resultNormal.y;
+            vertices[i + j + 5] = resultNormal.z;
+        }
+    }
+
+    int i, k;
+    for (i = 0, k = 0; i < (n - 1) * (rev - 1) * 6; k++) {
+        for (int j = 0; j < n - 1; j++, i += 6, k++) {
+            indices[i] = k;
+            indices[i + 1] = k + n;
+            indices[i + 2] = k + 1;
+            indices[i + 3] = k + n;
+            indices[i + 4] = k + 1 + n;
+            indices[i + 5] = k + 1;
+        }
+    }
+
+    for (int c = 0, k = n * (rev - 1); c < n - 1; c++, i += 6) {
+        indices[i] = c;
+        indices[i + 1] = c + k + 1;
+        indices[i + 2] = c + k;
+        indices[i + 3] = c;
+        indices[i + 4] = c + 1;
+        indices[i + 5] = c + k + 1;
+    }
+    /*for (int i = 1; i < points.size() - 1; i++) {
         auto vec1 = points[i] - points[i - 1];
         auto vec2 = points[i + 1] - points[i];
         vec1.normalize();
@@ -94,12 +167,17 @@ bool Body::createModel(const std::vector<Point2D> &points) {
         normals.emplace_back(normal.x, normal.y);
     }
     normals.push_back(normals[normals.size() - 1]);
+    auto v = points[n - 2] - points[n - 1];
+    v.normalize();
+    v.x *= -1.0f;
+    normals.emplace_back(v.x, v.y);
+    int x = normals.size();
 
     auto *vertices = new GLfloat[vertSize];
     for (size_t i = 0; i <= rev; i++) {
         auto model = glm::mat4(1.0f);
         auto rad = 360.0f * i / rev;
-        auto rotated = glm::rotate(model, glm::radians(rad), glm::vec3(0.0, 0.0, 0.0));
+        auto rotated = glm::rotate(model, glm::radians(rad), glm::vec3(1.0, 0.0, 0.0));
         for (size_t j = 0; j < n; j++) {
             size_t idx = 6 * (i * n + j);
             auto resultPos = rotated * glm::vec4(points[j].x, points[j].y, 0.0f, 1.0f);
@@ -124,7 +202,7 @@ bool Body::createModel(const std::vector<Point2D> &points) {
             indices[idx + 4] = n * (i + 1) + (j + 1);
             indices[idx + 5] = n * i + (j + 1);
         }
-    }
+    }*/
 
     glGenVertexArrays(1, &g_model.vao);
     glBindVertexArray(g_model.vao);
@@ -142,8 +220,7 @@ bool Body::createModel(const std::vector<Point2D> &points) {
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (const GLvoid *) 0);
     glEnableVertexAttribArray(1);
-    //(const GLvoid *) (3 * sizeof(GLfloat)
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (const GLvoid *) (const GLvoid *) (3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (const GLvoid *) (3 * sizeof(GLfloat)));
 
     delete[] vertices;
     delete[] indices;
